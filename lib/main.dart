e973306a7757725e07f54d4a0d3dbea0e6d2f7e6
Rafael Oliveira <raaf.forsoft.25@gmail.com>
2021-08-02 23:00:44 -0300

    create form to add new transfer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,6 +20,10 @@ class ByteBankApp extends StatelessWidget {
 }
 
 class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +33,7 @@ class FormularioTransferencia extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
+              controller: _controladorCampoNumeroConta,
               keyboardType: TextInputType.number,
               style: TextStyle(fontSize: 24.0),
               decoration: InputDecoration(
@@ -38,6 +45,7 @@ class FormularioTransferencia extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
+              controller: _controladorCampoValor,
               keyboardType: TextInputType.number,
               style: TextStyle(fontSize: 24.0),
               decoration: InputDecoration(
@@ -49,6 +57,17 @@ class FormularioTransferencia extends StatelessWidget {
           ),
           ElevatedButton(
               onPressed: () {
+                // debugPrint(_controladorCampoValor.text);
+                final int? numeroConta =
+                    int.tryParse(_controladorCampoNumeroConta.text);
+                final double? valor =
+                    double.tryParse(_controladorCampoValor.text);
+
+                if (numeroConta != null && valor != null) {
+                  final transferenciaCriada = Transferencia(valor, numeroConta);
+                  debugPrint('${transferenciaCriada}');
+                }
+                // debugPrint(_controladorCampoNumeroConta.text);
                 // debugPrint(_controladorCampoValor.text);
               },
               child: Text("Confirmar")),
@@ -102,4 +121,9 @@ class Transferencia {
   final int numeroConta;
 
   Transferencia(this.valor, this.numeroConta);
+
+  @override
+  String toString() {
+    return 'Transferencia { valor: $valor, numeroConta: $numeroConta }';
+  }
 }
