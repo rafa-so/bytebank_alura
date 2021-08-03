cc6953c04ef204aae1228853ce332e4548da8bd6
Rafael Oliveira <raaf.forsoft.25@gmail.com>
2021-08-02 22:44:01 -0300

    refactoring code and create form insert new transfer

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,82 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("transferências"),
+  runApp(ByteBankApp());
+}
+
+class ByteBankApp extends StatelessWidget {
+  const ByteBankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
       ),
-      body: ListaTransferencia(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Icon(Icons.add),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Criando Transferência')),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                labelText: "Número da Conta",
+                hintText: "0000",
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: "Valor",
+                hintText: "0.00",
+              ),
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                // debugPrint(_controladorCampoValor.text);
+              },
+              child: Text("Confirmar")),
+        ],
       ),
-    ),
-  ));
+    );
+  }
 }
 
 class ListaTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ItemTransferencia(Transferencia(200.0, 10010)),
-        ItemTransferencia(Transferencia(201.0, 10011)),
-        ItemTransferencia(Transferencia(202.0, 10012)),
-        ItemTransferencia(Transferencia(203.0, 10013)),
-      ],
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("transferências"),
+        ),
+        body: Column(
+          children: [
+            ItemTransferencia(Transferencia(200.0, 10010)),
+            ItemTransferencia(Transferencia(201.0, 10011)),
+            ItemTransferencia(Transferencia(202.0, 10012)),
+            ItemTransferencia(Transferencia(203.0, 10013)),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ));
   }
 }
 
