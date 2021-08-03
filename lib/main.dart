5d2eda7065957bcc1c0ad1fd90384c127fd05bd3
Rafael Oliveira <raaf.forsoft.25@gmail.com>
2021-08-02 21:40:09 -0300

    refactoring main dart file

--- lib/main.dart
+++ lib/main.dart
@@ -6,28 +6,49 @@ void main() {
       appBar: AppBar(
         title: Text("transferências"),
       ),
-      body: Column(
-        children: [
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text("100.0"),
-              subtitle: Text("1000"),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text("200.0"),
-              subtitle: Text("1000"),
-            ),
-          ),
-        ],
-      ),
+      body: ListaTransferencia(),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
         child: Icon(Icons.add),
       ),
     ),
   ));
+}
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(200.0, 10010)),
+        ItemTransferencia(Transferencia(201.0, 10011)),
+        ItemTransferencia(Transferencia(202.0, 10012)),
+        ItemTransferencia(Transferencia(203.0, 10013)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(this._transferencia.valor.toString()),
+        subtitle: Text(this._transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
